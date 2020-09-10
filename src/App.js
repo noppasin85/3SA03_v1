d83e4ae69a9be84dcfd3aad0e5b2b33a85d458d7
noppasin85 <topzaza_2011@hotmail.com>
2020-09-10 10:18:43 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CharacterCard from './CharacterCard';
+import WordCard from './wordCard'
+
+const word = "Hello";
+function App() {
+  return (
+    <div>
+      <WordCard value="hello"/>
+    </div>
+    );
+   }
+
+export default App;
